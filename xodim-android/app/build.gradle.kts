f2f6095ceee81289914eea63bbd10f0536b583dc
arximondo-cmd <arximondo@gmail.com>
2026-08-11 18:01:59 +0500

    Update build.gradle.kts

--- xodim-android/app/build.gradle.kts
+++ xodim-android/app/build.gradle.kts
@@ -1,13 +1,26 @@
-plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
 
 android {
     namespace = "uz.sunnat.sartarosh.xodim"
     compileSdk = 35
+
     defaultConfig {
         applicationId = "uz.sunnat.sartarosh.xodim"
         minSdk = 26
         targetSdk = 35
         versionCode = 1
         versionName = "1.0.0-alpha1"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
